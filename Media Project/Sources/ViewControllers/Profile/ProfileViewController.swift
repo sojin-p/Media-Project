3516fc11d67d052f228dc4ceecb1a10e74ee9d8f
sojin-p <luv2hong@gmail.com>
2023-08-29 21:41:09 +0900

    Delegate로 이름 값 전달

--- Media Project/Sources/ViewControllers/Profile/ProfileViewController.swift
+++ Media Project/Sources/ViewControllers/Profile/ProfileViewController.swift
@@ -7,9 +7,15 @@
 
 import UIKit
 
+protocol PassNicknameDelegate {
+    func receiveNickname(nickname: String)
+}
+
 class ProfileViewController: BaseViewController {
     
     let mainView = ProfileView()
+    var titleList = ["이름", "사용자 이름", "성별 대명사", "소개", "링크", "성별"]
+    var contentList: [String] = []
     
     override func loadView() {
         self.view = mainView
@@ -32,18 +38,41 @@ class ProfileViewController: BaseViewController {
 extension ProfileViewController: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return titleList.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
         guard let cell = tableView.dequeueReusableCell(withIdentifier: ProfileTableViewCell.identifier) as? ProfileTableViewCell else { return UITableViewCell() }
         
-        cell.titleLabel.text = "테스트입니다"
+        cell.titleLabel.text = titleList[indexPath.row]
+        
+        if contentList.isEmpty {
+            cell.textFieldLabel.text = titleList[indexPath.row]
+        } else {
+            cell.textFieldLabel.text = contentList[indexPath.row]
+            cell.textFieldLabel.textColor = .black
+        }
         
         return cell
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        navigationController?.pushViewController(textFieldViewController(), animated: true)
+        
+        let vc = TextFieldViewController()
+        
+        vc.delegate = self
+        
+        navigationController?.pushViewController(vc, animated: true)
     }
+}
+
+extension ProfileViewController: PassNicknameDelegate {
+    
+    func receiveNickname(nickname: String) {
+        print("받았나?", nickname)
+        contentList.insert(nickname, at: 0)
+        mainView.tableView.reloadRows(at: [IndexPath(row: 0, section: 0)], with: .none)
+    }
+    
 }
