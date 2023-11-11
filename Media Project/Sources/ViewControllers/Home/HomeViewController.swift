2e79af622ff931d9683e1a4523ecc9f29505fa05
Sojin Park <luv2hong@gmail.com>
2023-11-11 13:50:48 +0900

    [Test] HomeVC TableView Test

--- Media Project/Sources/ViewControllers/Home/HomeViewController.swift
+++ Media Project/Sources/ViewControllers/Home/HomeViewController.swift
@@ -6,12 +6,54 @@
 //
 
 import UIKit
+import SnapKit
 
 final class HomeViewController: BaseViewController {
     
+    private let tableView: UITableView = {
+        let view = UITableView()
+        view.register(HomeTableViewCell.self, forCellReuseIdentifier: HomeTableViewCell.identifier)
+        view.backgroundColor = .cyan
+        view.estimatedRowHeight = 300
+        view.rowHeight = UITableView.automaticDimension
+        view.separatorStyle = .none
+        return view
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .lightGray
+        print("====viewDidLoad")
+        view.backgroundColor = .white
+        
+        tableView.delegate = self
+        tableView.dataSource = self
     }
     
+    override func configureView() {
+        view.addSubview(tableView)
+    }
+    
+    override func setConstraints() {
+        tableView.snp.makeConstraints { make in
+            make.edges.equalTo(view.safeAreaLayoutGuide)
+        }
+    }
+    
+}
+
+extension HomeViewController: UITableViewDelegate, UITableViewDataSource{
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: HomeTableViewCell.identifier, for: indexPath) as? HomeTableViewCell else { return UITableViewCell() }
+        
+        cell.posterImageView.backgroundColor = .systemYellow
+        cell.testLabel.text = "\(indexPath)"
+        
+        return cell
+    }
+
 }
