d9a2484259589592526b50dfa90f58f687938065
Sojin Park <luv2hong@gmail.com>
2023-11-11 13:27:39 +0900

    [Add] HomeViewController 파일 추가

---
+++ Media Project/Sources/ViewControllers/Home/HomeViewController.swift
@@ -0,0 +1,17 @@
+//
+//  HomeViewController.swift
+//  Media Project
+//
+//  Created by 박소진 on 2023/11/11.
+//
+
+import UIKit
+
+final class HomeViewController: BaseViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .lightGray
+    }
+    
+}
