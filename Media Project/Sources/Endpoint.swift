32986573d06019c9784cbe47ab93002b02717d74
Sojin Park <luv2hong@gmail.com>
2023-11-11 13:25:58 +0900

    [Style] Endpoint 코드 스타일 변경

--- Media Project/Sources/Endpoint.swift
+++ Media Project/Sources/Endpoint.swift
@@ -14,12 +14,7 @@ enum Endpoint {
     case person
     
     var requestURL: String {
-        switch self {
-        case .movie: return URL.makeEndPointString("trending/movie/day?api_key=\(APIKey.tmdbKey)")
-        case .all: return URL.makeEndPointString("trending/all/day?api_key=\(APIKey.tmdbKey)")
-        case .tv: return URL.makeEndPointString("trending/tv/day?api_key=\(APIKey.tmdbKey)")
-        case .person: return URL.makeEndPointString("trending/person/day?api_key=\(APIKey.tmdbKey)")
-        }
+        return URL.makeEndPointString("trending/\(self)/day?api_key=\(APIKey.tmdbKey)")
 
     }
 }
