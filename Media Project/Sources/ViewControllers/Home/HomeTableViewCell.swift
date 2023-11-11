f466cf5e3298b086d4c79f146c18258c156dd2ec
Sojin Park <luv2hong@gmail.com>
2023-11-11 15:50:42 +0900

    [Design] HomeTableViewCell 디자인 변경

--- Media Project/Sources/ViewControllers/Home/HomeTableViewCell.swift
+++ Media Project/Sources/ViewControllers/Home/HomeTableViewCell.swift
@@ -9,8 +9,29 @@ import UIKit
 
 final class HomeTableViewCell: BaseTableViewCell {
     
+    let containerView = {
+        let view = UIView()
+        view.layer.cornerRadius = 20
+        view.layer.shadowColor = UIColor.lightGray.cgColor
+        view.layer.shadowOffset = CGSize(width: 3, height: 3) //햇빛 방향
+        view.layer.shadowRadius = 4 //퍼짐의 정도
+        view.layer.shadowOpacity = 0.3 //0~1 사이에서 투명도 조절
+        view.clipsToBounds = false
+        return view
+    }()
+    
     let posterImageView = {
         let view = UIImageView()
+        view.layer.cornerRadius = 20
+        return view
+    }()
+    
+    let posterBackImageView = {
+        let view = UIImageView()
+        view.image = Image.posterBackground
+        view.layer.cornerRadius = 20
+        view.alpha = 0.5
+        view.clipsToBounds = true
         return view
     }()
     
@@ -21,22 +42,42 @@ final class HomeTableViewCell: BaseTableViewCell {
         return view
     }()
     
+    let playButton = {
+        let view = UIButton()
+        view.setImage(UIImage(systemName: "heart.fill"), for: .normal)
+        view.backgroundColor = .white
+        return view
+    }()
+    
     override func configureView() {
-        contentView.addSubview(posterImageView)
-        contentView.addSubview(testLabel)
+        [containerView, testLabel].forEach {  contentView.addSubview($0) }
+        [posterImageView, posterBackImageView, playButton].forEach { containerView.addSubview($0) }
     }
     
     override func setConstraints() {
-        posterImageView.snp.makeConstraints { make in
+        containerView.snp.makeConstraints { make in
             make.verticalEdges.equalToSuperview().inset(20)
             make.width.equalToSuperview().multipliedBy(0.8)
-            make.height.equalTo(contentView.snp.width).multipliedBy(1.3)
+            make.height.equalTo(contentView.snp.width).multipliedBy(1.2)
             make.centerX.equalToSuperview()
+        }
+        
+        posterImageView.snp.makeConstraints { make in
+            make.edges.equalTo(containerView)
+        }
+        
+        posterBackImageView.snp.makeConstraints { make in
+            make.edges.equalTo(containerView)
         }
         
         testLabel.snp.makeConstraints { make in
             make.bottom.equalTo(posterImageView).offset(10)
             make.horizontalEdges.equalToSuperview()
+        }
+        
+        playButton.snp.makeConstraints { make in
+            make.size.equalTo(50)
+            make.bottom.trailing.equalToSuperview().inset(15)
         }
     }
 }
