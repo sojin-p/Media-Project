5a61a78ce68386413ddec7fd195f1e6561c8d85c
Sojin Park <luv2hong@gmail.com>
2023-11-11 17:36:15 +0900

    [Design] HomeCollectionViewCell 디자인 레아이웃 변경

--- Media Project/Sources/ViewControllers/Home/HomeCollectionViewCell.swift
+++ Media Project/Sources/ViewControllers/Home/HomeCollectionViewCell.swift
@@ -12,29 +12,17 @@ final class HomeCollectionViewCell: BaseCollectionViewCell {
     let posterImageView = {
         let view = UIImageView()
         view.backgroundColor = .systemPink
-        return view
-    }()
-    
-    let testLabel = {
-        let view = UILabel()
-        view.textAlignment = .center
-        view.text = "\(Int.random(in: 1...100))"
+        view.layer.cornerRadius = 10
         return view
     }()
     
     override func configureView() {
         contentView.addSubview(posterImageView)
-        contentView.addSubview(testLabel)
     }
     
     override func setConstraints() {
         posterImageView.snp.makeConstraints { make in
-            make.edges.equalToSuperview().inset(5)
-        }
-        
-        testLabel.snp.makeConstraints { make in
-            make.top.equalTo(posterImageView).offset(10)
-            make.horizontalEdges.equalToSuperview()
+            make.edges.equalToSuperview()
         }
     }
 }
