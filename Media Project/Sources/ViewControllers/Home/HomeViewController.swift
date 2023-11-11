bf9cf93997b85035fd2cae23fce074caf756f879
Sojin Park <luv2hong@gmail.com>
2023-11-11 14:47:54 +0900

    [Test] HomeVC 2번째 섹션 ColectionView Test

--- Media Project/Sources/ViewControllers/Home/HomeViewController.swift
+++ Media Project/Sources/ViewControllers/Home/HomeViewController.swift
@@ -13,9 +13,10 @@ final class HomeViewController: BaseViewController {
     private let tableView: UITableView = {
         let view = UITableView()
         view.register(HomeTableViewCell.self, forCellReuseIdentifier: HomeTableViewCell.identifier)
+        view.register(HomeMainTableViewCell.self, forCellReuseIdentifier: HomeMainTableViewCell.identifier)
         view.backgroundColor = .cyan
-        view.estimatedRowHeight = 300
-        view.rowHeight = UITableView.automaticDimension
+//        view.estimatedRowHeight = 300
+//        view.rowHeight = UITableView.automaticDimension
         view.separatorStyle = .none
         return view
     }()
@@ -43,17 +44,63 @@ final class HomeViewController: BaseViewController {
 
 extension HomeViewController: UITableViewDelegate, UITableViewDataSource{
     
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 20
+        switch section {
+        case 0: return 1
+        default: return 5
+        }
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: HomeTableViewCell.identifier, for: indexPath) as? HomeTableViewCell else { return UITableViewCell() }
         
-        cell.posterImageView.backgroundColor = .systemYellow
-        cell.testLabel.text = "\(indexPath)"
-        
-        return cell
+        switch indexPath.section {
+        case 0:
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: HomeTableViewCell.identifier, for: indexPath) as? HomeTableViewCell else { return UITableViewCell() }
+            
+            cell.posterImageView.backgroundColor = .systemYellow
+            cell.testLabel.text = "\(indexPath)"
+            
+            return cell
+            
+        default:
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: HomeMainTableViewCell.identifier, for: indexPath) as? HomeMainTableViewCell else { return UITableViewCell() }
+            
+            cell.collectionView.delegate = self
+            cell.collectionView.dataSource = self
+            
+            return cell
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        switch indexPath.section {
+        case 0: return UITableView.automaticDimension
+        default: return 170
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
+        switch indexPath.section {
+        case 0: return 300
+        default: return 170
+        }
     }
 
+}
+
+
+extension HomeViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: HomeCollectionViewCell.identifier, for: indexPath) as? HomeCollectionViewCell else { return UICollectionViewCell() }
+        return cell
+    }
 }
