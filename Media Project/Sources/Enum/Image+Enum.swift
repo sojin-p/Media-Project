f59474dac5a3f95a0b0e648ae34a3b8d38954b8f
Sojin Park <luv2hong@gmail.com>
2023-11-11 15:49:28 +0900

    [Add] Image+Enum 파일 추가

---
+++ Media Project/Sources/Enum/Image+Enum.swift
@@ -0,0 +1,12 @@
+//
+//  Image+Enum.swift
+//  Media Project
+//
+//  Created by 박소진 on 2023/11/11.
+//
+
+import UIKit
+
+enum Image {
+    static let posterBackground = UIImage(resource: .background).withRenderingMode(.alwaysOriginal).withTintColor(.white)
+}
