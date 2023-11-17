ebfbb33315dc9cf3f572258dfed50d0927d69289
Sojin Park <luv2hong@gmail.com>
2023-11-17 22:49:57 +0900

    [Design] HomeCollectionViewCell 디자인 변경

--- Media Project/Sources/ViewControllers/Home/HomeCollectionViewCell.swift
+++ Media Project/Sources/ViewControllers/Home/HomeCollectionViewCell.swift
@@ -9,18 +9,34 @@ import UIKit
 
 final class HomeCollectionViewCell: BaseCollectionViewCell {
     
+    let backView = {
+        let view = UIView()
+        view.layer.shadowColor = UIColor.lightGray.cgColor
+        view.layer.shadowOffset = CGSize(width: 2, height: 2)
+        view.layer.shadowRadius = 2
+        view.layer.shadowOpacity = 0.5
+        return view
+    }()
+    
     let posterImageView = {
         let view = UIImageView()
-        view.backgroundColor = .systemPink
+        view.backgroundColor = .lightGray
+        view.contentMode = .scaleAspectFill
         view.layer.cornerRadius = 10
+        view.clipsToBounds = true
         return view
     }()
     
     override func configureView() {
-        contentView.addSubview(posterImageView)
+        contentView.addSubview(backView)
+        backView.addSubview(posterImageView)
     }
     
     override func setConstraints() {
+        backView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+        
         posterImageView.snp.makeConstraints { make in
             make.edges.equalToSuperview()
         }
