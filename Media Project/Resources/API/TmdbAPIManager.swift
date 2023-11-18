44b5c42d4f1fc3a03ea77ee33f79e5f968e9deae
Sojin Park <luv2hong@gmail.com>
2023-11-18 14:44:01 +0900

    [Chore] TmdbAPIManager 쓰지 않는 코드 주석 처리

--- Media Project/Resources/API/TmdbAPIManager.swift
+++ Media Project/Resources/API/TmdbAPIManager.swift
@@ -34,8 +34,6 @@ class TmdbAPIManager {
     static let shared = TmdbAPIManager()
     private init() { }
     
-    let parameters: Parameters = ["language": "ko"]
-    
     func requestConvertible<T: Decodable>(type: T.Type, api: Router, comletion: @escaping (Result<T, NetworkError>) -> () ) {
         
         AF.request(api).validate(statusCode: 200...500)
@@ -44,11 +42,14 @@ class TmdbAPIManager {
                 case .success(let data): comletion(.success(data))
                 case .failure(_):
                     let statusCode = response.response?.statusCode ?? 500
+                    print("==statusCode==",statusCode)
                     guard let error = NetworkError(rawValue: statusCode) else { return }
                     comletion(.failure(error))
                 }
             }
     }
+    
+    let parameters: Parameters = ["language": "ko"]
 
     func callRequest(type: Endpoint, page: Int, completionHandler: @escaping (AllTrend?) -> Void ) {
 
@@ -99,31 +100,31 @@ class TmdbAPIManager {
 //        }
     }
 
-    func callCreditRequest(id: Int, completionHandler: @escaping ([Cast]) -> () ) {
-        let url = URL(string: "https://api.themoviedb.org/3/movie/\(id)/credits?api_key=\(APIKey.tmdbKey)")
-        var cast: [Cast] = []
-
-        AF.request(url!, method: .get).validate().responseJSON { response in
-            switch response.result {
-            case .success(let value):
-                let json = JSON(value)
-//                print("JSON: \(json)")
-
-                for i in json["cast"].arrayValue {
-                    let name = i["original_name"].stringValue
-                    let character = i["character"].stringValue
-                    let profile = URL.imageURL+i["profile_path"].stringValue
-
-                    let data = Cast(originalName: name, character: character, profileURLString: profile)
-                    cast.append(data)
-                }
-                completionHandler(cast)
-
-            case .failure(let error):
-                print(error)
-            }
-        }
-    }
+//    func callCreditRequest(id: Int, completionHandler: @escaping ([Cast]) -> () ) {
+//        let url = URL(string: "https://api.themoviedb.org/3/movie/\(id)/credits?api_key=\(APIKey.tmdbKey)")
+//        var cast: [Cast] = []
+//
+//        AF.request(url!, method: .get).validate().responseJSON { response in
+//            switch response.result {
+//            case .success(let value):
+//                let json = JSON(value)
+////                print("JSON: \(json)")
+//
+//                for i in json["cast"].arrayValue {
+//                    let name = i["original_name"].stringValue
+//                    let character = i["character"].stringValue
+//                    let profile = URL.imageURL+i["profile_path"].stringValue
+//
+//                    let data = Cast(originalName: name, character: character, profileURLString: profile)
+//                    cast.append(data)
+//                }
+//                completionHandler(cast)
+//
+//            case .failure(let error):
+//                print(error)
+//            }
+//        }
+//    }
 
     func callSimilarRequset(id: Int, completionHandler: @escaping (MovieTrend) -> () ) {
         
