5b6b028c262acdabf34ae4bc51f52554ae5e2016
Sojin Park <luv2hong@gmail.com>
2023-11-18 14:42:39 +0900

    [Fix] Cast Model Codable 구조로 수정

--- Media Project/Sources/Models/Cast.swift
+++ Media Project/Sources/Models/Cast.swift
@@ -7,12 +7,25 @@
 
 import Foundation
 
-struct Cast {
-    let originalName: String
-    let character: String
-    let profileURLString: String
+struct CastData: Decodable {
+    let id: Int
+    var cast, crew: [Cast]
+}
+
+struct Cast: Decodable {
+    let id: Int
+    let name: String //배우이름
+    let character: String? //역할 이름
+    let originalName: String //배우의 오리지날 이름
+    let knownForDepartment: String
+    let profilePath: String?
+    let castId: Int?
     
-    var castTitle: String {
-        "\(character) (\(originalName))"
+    enum CodingKeys: String, CodingKey {
+        case id, name, character
+        case originalName = "original_name"
+        case knownForDepartment = "known_for_department"
+        case profilePath = "profile_path"
+        case castId = "cast_id"
     }
 }
